ae243e8ced9a66dc0d1ec69b8490b1091a577ed5
SpencerHillhouse <shillhouse@basecampcodingacademy.org>
2025-01-09 17:31:53 -0600

    This is a test

--- app/views.py
+++ app/views.py
@@ -13,6 +13,7 @@ from .forms import CreateUserForm
 from .decorators import unathenticated_user, allowed_users, admin_only
 
 # Create your views here.
+# This is a test.
 
 @login_required(login_url='login')
 @admin_only
